1dc7cfd026e7e0c97875f1c5da0b9e1689a3d7ff
Wolfgang Frisch <wolfgang.frisch@suse.com>
2025-10-21 16:52:45 +0200

    demo: malloc was optimized out by gcc -O2
    I accidentally ran the test mentioned in the README before adding -O2 to
    CFLAGS. Now it should work again.

--- demo/malloc.c
+++ demo/malloc.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <memory.h>
+
+
+void print_usage(const char* prog_name) {
+	fprintf(stderr, "usage: %s SIZE\n", prog_name);
+}
+
 
 int main(int argc, char** argv) {
+	int len;
+	char* buf;
+
 	if (argc != 2) {
-		fprintf(stderr, "usage: %s SIZE\n", argv[0]);
+		print_usage(argv[0]);
 		return 1;
 	}
-	char* foo = malloc(atoi(argv[1]));
-	if (!foo) {
-		fprintf(stderr, "malloc failed\n");
+
+	len = atoi(argv[1]);
+	if (len < 0) {
+		print_usage(argv[0]);
 		return 1;
 	}
+
+	buf = malloc(len);
+	if (!buf) {
+		perror("malloc");
+		return 1;
+	}
+
+	memset(buf, 0, len);
+	printf("%s", buf);  // memset would be optimized out otherwise
+
 	return 0;
 }
